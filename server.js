3aa021ace195f69567a2fd2e2106a02b19f57c4e
Damian Norman <damiannorman@gmail.com>
2020-04-02 13:15:24 -0400

    added compression

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ modPath.addPath(`${__dirname}`);
 const express = require('express');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
+const compression = require('compression');
 
 const cors = require('cors');
 const petsRoute = require('routes/pets');
@@ -12,10 +13,11 @@ const tryToCatch = require('try-to-catch');
 module.exports = async ({serverPort}) => {
 	const {urlencoded, json} = bodyParser;
 	const app = express();
-	app.use(cors());
-
 
 	app.use(helmet());
+	app.use(cors());
+	app.use(compression());
+
 	app.use('/api/pets', petsRoute);
 	app.use(urlencoded && urlencoded({
 		extended: false
